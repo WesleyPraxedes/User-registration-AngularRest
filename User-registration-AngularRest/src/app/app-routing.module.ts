93a157be1e737c7231db4bfd624cf5927e309163
Wesley Praxedes <wesleyprofile@gmail.com>
2024-01-16 21:40:43 -0300

    Created exclusive module for routing

---
+++ User-registration-AngularRest/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+  // Adicione outras rotas conforme necessário
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- User-registration-AngularRest/src/app/app.module.ts
+++ User-registration-AngularRest/src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
-
-
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
-  // Adicione outras rotas conforme necessário
-];
-
-
 
 @NgModule({
   declarations: [
@@ -28,7 +19,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule, // Importe o AppRoutingModule aqui
   ],
   providers: [],
   bootstrap: [AppComponent]
